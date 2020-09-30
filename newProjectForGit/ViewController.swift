a764800f2afa852b6a1cc45a2f6da6c01ea96138
Pravin <pravin.ramana.127@k12.friscoisd.org>
2020-09-30 11:55:49 -0500

    Initial Commit

---
+++ newProjectForGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newProjectForGit
+//
+//  Created by Ramana, Pravin on 9/30/20.
+//  Copyright © 2020 Ramana, Pravin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
